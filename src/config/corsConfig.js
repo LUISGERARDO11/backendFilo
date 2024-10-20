aa8fea92e47513ba7f2a4b4fbba6ae80fa28485c
FiloDev172 <filograficosdev@gmail.com>
2024-10-19 22:52:09 -0600

    Update corsConfig.js

--- src/config/corsConfig.js
+++ src/config/corsConfig.js
@@ -14,7 +14,7 @@ const corsOptions = {
     }
   },
   methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
-  allowedHeaders: ['Content-Type', 'Authorization','Access-Control-Allow-Origin'],
+  allowedHeaders: ['Content-Type', 'Authorization'],
   credentials: true,
   optionsSuccessStatus: 200
 };
