26e37d8127df63a1d93e190ce27f4cc9f92dfe70
FiloDev172 <filograficosdev@gmail.com>
2024-10-24 00:55:40 -0600

    Update emailTypeRoutes.js

--- src/routes/emailTypeRoutes.js
+++ src/routes/emailTypeRoutes.js
@@ -10,19 +10,19 @@ const tokenExpirationMiddleware = require('../middlewares/verifyTokenExpiration'
 //Importar controladores 
 const emailTypeController = require('../controllers/emailTypeController');
 
-// Ruta para obtener crear un tipo de correo
+// Ruta para obtener todos los usuarios con su sesión más reciente 
 router.post('/', authMiddleware, tokenExpirationMiddleware.verifyTokenExpiration,roleMiddleware(['administrador']), emailTypeController.createEmailType);
 
-// Ruta para obtener un tipo de correo por su id
+// Ruta para que un administrador pueda eliminar un cliente
 router.get('/:id', authMiddleware, tokenExpirationMiddleware.verifyTokenExpiration, roleMiddleware(['administrador']), emailTypeController.getEmailTypeById);
 
-// Ruta para obtener todos los tipos de email
+// Ruta para que un administrador pueda eliminar un cliente
 router.get('/', authMiddleware, tokenExpirationMiddleware.verifyTokenExpiration, roleMiddleware(['administrador']), emailTypeController.getAllEmailTypes);
 
-// Ruta para actualizar un tipo de email (se puede cambiar uno o más campos)
+// Ruta para actualizar el número máximo de intentos fallidos de login en todas las cuentas
 router.put('/:id', authMiddleware, tokenExpirationMiddleware.verifyTokenExpiration,  roleMiddleware(['administrador']), emailTypeController.updateEmailType);
 
-// Ruta para eliminar lógicamente un tipo de email
+// Ruta para actualizar el número máximo de intentos fallidos de login en todas las cuentas
 router.delete('/:id', authMiddleware, tokenExpirationMiddleware.verifyTokenExpiration,  roleMiddleware(['administrador']), emailTypeController.deleteEmailType);
 
 module.exports = router; 
