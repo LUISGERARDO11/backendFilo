182450bc097c045b88482fb9de36692481fa9a27
LUISGERARDO11 <luis34julieta@gmail.com>
2024-11-28 11:45:12 -0600

    actualizacion del auth middleware para redirigir al login en caso de que no cuente con una cookie

--- src/middlewares/authMiddleware.js
+++ src/middlewares/authMiddleware.js
@@ -2,39 +2,52 @@
 JWT token stored in a cookie. Here's a breakdown of what the code does: */
 const jwt = require("jsonwebtoken");
 require("dotenv").config();
-//Importar modelos
-const Session = require("../models/Session");
-const User = require("../models/User");
-//Importar servicios
-const authService = require("../services/authService");
 
-// Middleware para verificar la autenticación del token JWT desde cookies
 // Middleware para verificar la autenticación del token JWT desde cookies
 const authMiddleware = (req, res, next) => {
   const token = req.cookies['token']; // Extraer el token de la cookie
   const secret = process.env.JWT_SECRET;
-  
+
   console.log('Token:', token); // Log del token
   console.log('Todas las cookies:', req.cookies);
   console.log('JWT_SECRET:', secret); // Log del secreto
-  
+
+  // URLs base para redirección
+  const baseUrls = {
+    development: [
+      'http://localhost:3000',
+      'http://localhost:4200',
+      'http://127.0.0.1:4200',
+      'http://127.0.0.1:3000',
+    ],
+    production: ['https://web-filograficos.vercel.app'],
+  };
+
+  const currentEnv = baseUrls[process.env.NODE_ENV]
+    ? process.env.NODE_ENV
+    : 'development';
+  const loginUrl = `${baseUrls[currentEnv][0]}/login`;
+
+  // Si no hay token, redirige al usuario a la página de inicio de sesión
   if (!token) {
-    return res.status(401).json({ message: "Acceso no autorizado. Token no proporcionado." });
+    console.warn("Token no proporcionado. Redirigiendo a la página de inicio de sesión...");
+    return res.redirect(loginUrl);
   }
 
   try {
-    jwt.verify(token, process.env.JWT_SECRET, (err, decoded) => {
+    jwt.verify(token, secret, (err, decoded) => {
       if (err) {
-         console.error('Error de verificación del token:', err);
-          return res.sendStatus(403); // Token inválido o error de verificación
+        console.error('Error de verificación del token:', err);
+        return res.sendStatus(403); // Token inválido o error de verificación
       }
       req.user = decoded; // Guarda el usuario decodificado en el objeto de la solicitud
       console.log('Usuario decodificado:', req.user);
       next(); // Continúa con el siguiente middleware
     });
   } catch (err) {
+    console.error("Error al procesar el token:", err);
     return res.status(401).json({ message: "Token inválido o expirado" });
   }
-}
+};
 
 module.exports = authMiddleware;
